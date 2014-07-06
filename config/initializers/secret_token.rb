7353e41b944b6c58ef0c0d0821ea31b811a866fd
Ming Chen <ming.chen@amaysim.com.au>
2014-07-06 10:15:53 +1000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NinefoldTest::Application.config.secret_token = '3b9cf24c6415dc56c781d51223d1b1632cb17a1993c1e464546b41d04572d6f9a058548e6d35a43ab344e05984cfb0c6f10c50424774a3a4bf7c7a0818fa5444'
